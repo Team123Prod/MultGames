fb3145e31e528dfbfb9ef91808b06c58e475c053
Sniper <Sniper@DESKTOP-9Q45AK4>
2017-12-05 22:11:58 +0200

    Добавьте файлы проекта.

---
+++ MultGames/Helpers/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameRoomsAPI.Helpers
+{
+    public class GameState
+    {
+        static public List<int[]> winnerIndexes = new List<int[]> { new int[] { 0,1,2}, new int[] { 3, 4, 5 }, new int[] { 6, 7, 8 },
+                                                            new int[] { 0,3,6},new int[] { 1,4,7},new int[] { 2,5,8},
+                                                            new int[] { 0,4,8},new int[] { 2,4,6}
+        };
+        static public Dictionary<int,GameStateLocal> gameState = new Dictionary<int, GameStateLocal>();
+    }
+
+    public class GameStateLocal
+    {
+        public string[] players = new string[] { "red", "blue" };
+        public int whosNext = 0;
+        public int numPlayers = 0;
+        public bool winnerPresent = false;
+        public int[] board = new int[] {
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0
+                };
+    }  
+}
---
+++ MultGames/Helpers/WebSocket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSocketSharp.Server;
+
+namespace GameRoomsAPI.Helpers
+{
+    public class WebSocket
+    {
+        static public WebSocketServer WS;
+        static public void Start()
+        {
+            WS = new WebSocketServer("ws://localhost:8081/");
+            WS.Start();
+        }
+    }
+}
